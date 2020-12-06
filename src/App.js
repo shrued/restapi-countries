36477e25166e5589ba64c816611b60f970563d2e
shrued <shrutisara10@gmail.com>
2020-12-06 19:25:19 +0530

    Paginated content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-} from "react-router-dom";
-import Temp from "./components/temp/index";
+
+import Page from "./components/page/index";
 
 class App extends Component {
   render() {
-    return (
-      <Router>
-        <Switch>
-          <Route path="/countries" component={Temp} />
-          <Route path="/">
-            <Redirect to="/countries" />
-          </Route>
-        </Switch>
-      </Router>
-    );
+    return <Page />;
   }
 }
 export default App;
--- src/components/page/index.js
+++ src/components/page/index.js
@@ -1,11 +1,106 @@
 import React, { Component } from "react";
-import Temp from "../temp/index";
+import axios from "axios";
+import ReactPaginate from "react-paginate";
 
 class Page extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      offset: 0,
+      tableData: [],
+      orgtableData: [],
+      perPage: 5,
+      currentPage: 0,
+    };
+    this.handlePageClick = this.handlePageClick.bind(this);
+  }
+
+  handlePageClick = (e) => {
+    const selectedPage = e.selected;
+    const offset = selectedPage * this.state.perPage;
+
+    this.setState(
+      {
+        currentPage: selectedPage,
+        offset: offset,
+      },
+      () => {
+        this.loadMoreData();
+      }
+    );
+  };
+
+  loadMoreData() {
+    const data = this.state.orgtableData;
+
+    const slice = data.slice(
+      this.state.offset,
+      this.state.offset + this.state.perPage
+    );
+    this.setState({
+      pageCount: Math.ceil(data.length / this.state.perPage),
+      tableData: slice,
+    });
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData() {
+    axios.get("https://restcountries.eu/rest/v2/all").then((res) => {
+      const data = res.data;
+      const slice = data.slice(
+        this.state.offset,
+        this.state.offset + this.state.perPage
+      );
+
+      this.setState({
+        pageCount: Math.ceil(data.length / this.state.perPage),
+        orgtableData: res.data,
+        tableData: slice,
+      });
+    });
+  }
+
   render() {
     return (
       <>
-        <Temp />
+        <table border="1">
+          <thead>
+            <th>Name</th>
+            <th>Alpha2Code</th>
+            <th>Population</th>
+            <th>CurrencyCode</th>
+          </thead>
+          <tbody>
+            {this.state.tableData.map((tdata, index) => (
+              <tr>
+                <td>{tdata.name}</td>
+                <td>{tdata.alpha2Code}</td>
+                <td>{tdata.population}</td>
+                <td>
+                  {tdata.currencies.map((cdata, index) => (
+                    <p>{cdata.code}&nbsp;</p>
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <ReactPaginate
+          previousLabel={"«"}
+          nextLabel={"»"}
+          breakLabel={"..."}
+          breakClassName={"break-me"}
+          pageCount={this.state.pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={this.handlePageClick}
+          containerClassName={"pagination"}
+          subContainerClassName={"pages pagination"}
+          activeClassName={"active"}
+        />
       </>
     );
   }
